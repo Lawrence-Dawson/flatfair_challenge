3b87cb3bff2f0c6d94531d27e8c1f756f7792d7e
Lawrence Dawson <ld@mettrr.com>
2019-06-25 16:38:08 +0100

    write initial test for getting config property

---
+++ tests/Unit/OrganizationUnitTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class OrganisationUnitConfigTest extends TestCase {
+    
+    /**
+    * @test
+    */
+    public function it_can_return_if_fixed_membership_fee()
+    {
+        $hasFixedMembershipFee = false;
+        $config = new OrganisationUnitConfig($hasFixedMembershipFee);
+
+        $this->assertEquals($hasFixedMembershipFee, $config->isFixedMembershipFee());
+    }
+}
+
